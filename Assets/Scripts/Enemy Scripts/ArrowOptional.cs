1c01bf833b989d9ea15f6640a3bc4dbc9dd052c0
Bennoooo <35964003+Bennoooo@users.noreply.github.com>
2018-02-01 10:54:32 +0100

    .
    .

--- Assets/Scripts/Enemy Scripts/ArrowOptional.cs
+++ Assets/Scripts/Enemy Scripts/ArrowOptional.cs
@@ -9,41 +9,83 @@ public class ArrowOptional : MonoBehaviour {
     public float timeDown;
     private float lastTurn;
     private bool up;
+    public float waitTime;
+    private float lastLanding;
+    private float timeUp2;
+
+    private bool arrowThrow;
     
     public Transform arrowSpawn;
+
+    public GameObject arrow;
+    private SpriteRenderer arrowMesh;
+    private BoxCollider2D arrowCollider;
 
 	
 	void Start ()
     {
+        timeUp2 = timeUp;
         lastTurn = Time.time;
+        lastLanding = Time.time - waitTime;
         up = true;
-        
-	}
+
+        arrowMesh = arrow.GetComponent<SpriteRenderer>();
+        arrowCollider = arrow.GetComponent<BoxCollider2D>();
+
+    }
 	
 	
 	void Update ()
     {
-        if (Time.time - lastTurn <= timeUp && up)
+        if (Time.time - lastLanding <= waitTime)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(target.position.x / (timeUp * 2), forceUp);
+            arrowThrow = false;
+            transform.position = new Vector2(arrowSpawn.position.x, arrowSpawn.position.y);
+
+            arrowCollider.enabled = false;
+            arrowMesh.enabled = false;
+        }
+        else
+        {
+            arrowCollider.enabled = true;
+            arrowMesh.enabled = true;
+            arrowThrow = true;
+        }
+
+        if (Time.time - lastTurn <= timeUp && up && arrowThrow)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2((target.position.x - transform.position.x) / 2, forceUp);
             
         }
-        else if (Time.time - lastTurn > timeUp && up)
+        else if (Time.time - lastTurn > timeUp && up && arrowThrow)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(target.position.x - transform.position.x, -transform.position.y + target.position.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2((target.position.x - transform.position.x) / timeDown, (-transform.position.y + target.position.y) / timeDown);
             up = false;
             lastTurn = Time.time;
         }
 
-        if (Time.time - lastTurn >= timeDown && !up)
+        if (Time.time - lastTurn >= timeDown && !up && arrowThrow)
         {
 
             transform.position = new Vector2(arrowSpawn.position.x, arrowSpawn.position.y);
             up = true;
             lastTurn = Time.time;
-            
+            lastLanding = Time.time;
+            timeUp = timeUp2 + waitTime;
+
         }
         
 
 	}
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (arrowThrow)
+        {
+         transform.position = new Vector2(arrowSpawn.position.x, arrowSpawn.position.y);
+         up = true;
+         lastTurn = Time.time;
+         lastLanding = Time.time;
+         timeUp = timeUp2 + waitTime;
+        }
+    }
 }
