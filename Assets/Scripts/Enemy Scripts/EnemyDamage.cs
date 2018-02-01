ee7b0de16125d08be8483d0482351be205990222
Bennoooo <35964003+Bennoooo@users.noreply.github.com>
2018-02-01 10:45:15 +0100

    enemy
    enemy

---
+++ Assets/Scripts/Enemy Scripts/EnemyDamage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyDamage : MonoBehaviour {
+
+    public float Health;
+    public float Damage;
+
+    public void TakeDamage()
+    {
+        Health -= Damage;
+        if (Health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
